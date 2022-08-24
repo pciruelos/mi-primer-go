a6aff341aeb0cfb3ee2147344a7edc46a563958f
pciruelos <pciruelos@yahoo.com>
2022-08-24 09:22:20 +0200

    second commmit

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/google/uuid"
+)
 
 type User struct {
+	Id       string
 	Name     string
 	Lastname string
 }
@@ -16,19 +21,28 @@ func handleUser(c *fiber.Ctx) error {
 }
 
 func createUser(c *fiber.Ctx) error {
-	user := User{
-		c.BodyParser(&user); err != nil
+	user := User{}
+	if err := c.BodyParser(&user); err != nil {
+		return err
 	}
+	user.Id = uuid.NewString()
 	return c.Status(fiber.StatusOK).JSON(user)
 }
 
 func main() {
 	app := fiber.New()
 
+	//middlewares
+	app.Use(logger.New())
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("hola")
 	})
-	app.Get("/user", handleUser)
+
+	userGroup := app.Group("/user")
+
+	userGroup.Get("", handleUser)
+
 	app.Post("/create", createUser)
 
 	app.Listen(":3000")
